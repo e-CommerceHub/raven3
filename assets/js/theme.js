623ea90bca1b5d0f23a523357bf0f00a6807a888
Patricio Monpelat <patricio.monpelat@gmail.com>
2023-06-15 17:03:25 -0300

    update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,19 +1,18 @@
 function theme_set(toggled) {
-  document.getElementById('style-light').disabled = toggled;
-  document.getElementById('style-dark').disabled = !toggled;
-  localStorage.setItem('theme-toggled', toggled ? '1' : '');
+  document.getElementById("style-light").disabled = toggled;
+  document.getElementById("style-dark").disabled = !toggled;
+  localStorage.setItem("theme-toggled", toggled ? "1" : "");
 }
 
 function theme_toggle() {
-  theme_set(!document.getElementById('style-light').disabled);
+  theme_set(!document.getElementById("style-light").disabled);
 }
 
-theme_set(localStorage.getItem('theme-toggled'));
+theme_set(localStorage.getItem("theme-toggled"));
 
-
-let preloader = document.getElementById('preloader');
+let preloader = document.getElementById("preloader");
 if (preloader) {
- window.addEventListener("load", () => {
-   preloader.remove();
- });
+  window.addEventListener("load", () => {
+    preloader.remove();
+  });
 }
