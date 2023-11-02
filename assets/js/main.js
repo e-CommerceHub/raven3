6edae8a9df9fed9f90299a4f317c39f02ef0ab9d
Patricio Monpelat <patricio.monpelat@gmail.com>
2023-11-02 12:10:13 -0300

    sound

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (function () {
-  'use strict';
+  "use strict";
 
   // const cursor = document.querySelector('.cursor');
   // const links = document.querySelectorAll('nav ul li a');
@@ -117,13 +117,13 @@
    * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener);
+    el.addEventListener("scroll", listener);
   };
 
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true);
+  let navbarlinks = select("#navbar .scrollto", true);
   const navbarlinksActive = () => {
     let position = window.scrollY + 200;
     navbarlinks.forEach((navbarlink) => {
@@ -134,13 +134,13 @@
         position >= section.offsetTop &&
         position <= section.offsetTop + section.offsetHeight
       ) {
-        navbarlink.classList.add('active');
+        navbarlink.classList.add("active");
       } else {
-        navbarlink.classList.remove('active');
+        navbarlink.classList.remove("active");
       }
     });
   };
-  window.addEventListener('load', navbarlinksActive);
+  window.addEventListener("load", navbarlinksActive);
   onscroll(document, navbarlinksActive);
 
   //
@@ -158,60 +158,60 @@
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
-    let header = select('#header');
+    let header = select("#header");
     let offset = header.offsetHeight;
 
     let elementPos = select(el).offsetTop;
     window.scrollTo({
       top: elementPos - offset,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let selectHeader = select('#header');
+  let selectHeader = select("#header");
   if (selectHeader) {
     const headerScrolled = () => {
       if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled');
+        selectHeader.classList.add("header-scrolled");
       } else {
-        selectHeader.classList.remove('header-scrolled');
+        selectHeader.classList.remove("header-scrolled");
       }
     };
-    window.addEventListener('load', headerScrolled);
+    window.addEventListener("load", headerScrolled);
     onscroll(document, headerScrolled);
   }
 
   /**
    * Back to top button
    */
-  let backtotop = select('.back-to-top');
+  let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
-        backtotop.classList.add('active');
+        backtotop.classList.add("active");
       } else {
-        backtotop.classList.remove('active');
+        backtotop.classList.remove("active");
       }
     };
-    window.addEventListener('load', toggleBacktotop);
+    window.addEventListener("load", toggleBacktotop);
     onscroll(document, toggleBacktotop);
   }
 
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function (e) {
-    select('#navbar').classList.toggle('navbar-mobile');
-    this.classList.toggle('bi-list');
-    this.classList.toggle('bi-x');
-    let burger = select('#burger');
-    if (burger.classList.contains('bi-list')) {
-      document.body.style.overflowY = 'visible';
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+    let burger = select("#burger");
+    if (burger.classList.contains("bi-list")) {
+      document.body.style.overflowY = "visible";
     } else {
-      document.body.style.overflowY = 'hidden';
+      document.body.style.overflowY = "hidden";
     }
   });
 
@@ -219,12 +219,12 @@
    * Mobile nav dropdowns activate
    */
   on(
-    'click',
-    '.navbar .dropdown > a',
+    "click",
+    ".navbar .dropdown > a",
     function (e) {
-      if (select('#navbar').classList.contains('navbar-mobile')) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
         e.preventDefault();
-        this.nextElementSibling.classList.toggle('dropdown-active');
+        this.nextElementSibling.classList.toggle("dropdown-active");
       }
     },
     true
@@ -234,18 +234,18 @@
    * Scrool with ofset on links with a class name .scrollto
    */
   on(
-    'click',
-    '.scrollto',
+    "click",
+    ".scrollto",
     function (e) {
       if (select(this.hash)) {
         e.preventDefault();
 
-        let navbar = select('#navbar');
-        if (navbar.classList.contains('navbar-mobile')) {
-          navbar.classList.remove('navbar-mobile');
-          let navbarToggle = select('.mobile-nav-toggle');
-          navbarToggle.classList.toggle('bi-list');
-          navbarToggle.classList.toggle('bi-x');
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
         }
         scrollto(this.hash);
       }
@@ -256,7 +256,7 @@
   /**
    * Scroll with ofset on page load with hash links in the url
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
         scrollto(window.location.hash);
@@ -267,9 +267,9 @@
   /**
    * Preloader
    */
-  let preloader = select('#preloader');
+  let preloader = select("#preloader");
   if (preloader) {
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
       preloader.remove();
     });
   }
@@ -277,17 +277,17 @@
   /**
    * Clients Slider
    */
-  new Swiper('.clients-slider', {
+  new Swiper(".clients-slider", {
     speed: 400,
     loop: true,
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
+      el: ".swiper-pagination",
+      type: "bullets",
       clickable: true,
     },
     breakpoints: {
@@ -313,29 +313,29 @@
   /**
    * Porfolio isotope and filter
    */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".portfolio-container");
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
+        itemSelector: ".portfolio-item",
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select("#portfolio-flters li", true);
 
       on(
-        'click',
-        '#portfolio-flters li',
+        "click",
+        "#portfolio-flters li",
         function (e) {
           e.preventDefault();
           portfolioFilters.forEach(function (el) {
-            el.classList.remove('filter-active');
+            el.classList.remove("filter-active");
           });
-          this.classList.add('filter-active');
+          this.classList.add("filter-active");
 
           portfolioIsotope.arrange({
-            filter: this.getAttribute('data-filter'),
+            filter: this.getAttribute("data-filter"),
           });
-          portfolioIsotope.on('arrangeComplete', function () {
+          portfolioIsotope.on("arrangeComplete", function () {
             AOS.refresh();
           });
         },
@@ -348,13 +348,13 @@
    * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox',
+    selector: ".portfolio-lightbox",
   });
 
   /**
    * Portfolio details slider
    */
-  new Swiper('.portfolio-details-slider', {
+  new Swiper(".portfolio-details-slider", {
     speed: 400,
     loop: true,
     autoplay: {
@@ -362,8 +362,8 @@
       disableOnInteraction: false,
     },
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
+      el: ".swiper-pagination",
+      type: "bullets",
       clickable: true,
     },
   });
@@ -371,17 +371,17 @@
   /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
+  new Swiper(".testimonials-slider", {
     speed: 600,
     loop: true,
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
+      el: ".swiper-pagination",
+      type: "bullets",
       clickable: true,
     },
   });
@@ -389,14 +389,35 @@
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       once: true,
       mirror: false,
     });
   });
+
+  /**
+   * Mute Button
+   */
+
+  const audioPlayer = document.getElementById("audio-player");
+  const muteButton = document.getElementById("mute-button");
+
+  // Function to toggle mute/unmute
+  function toggleMute() {
+    if (audioPlayer.muted) {
+      audioPlayer.muted = false;
+      muteButton.textContent = "🔊"; // Speaker icon
+    } else {
+      audioPlayer.muted = true;
+      muteButton.textContent = "🔇"; // Muted speaker icon
+    }
+  }
+
+  // Event listener for mute button click
+  muteButton.addEventListener("click", toggleMute);
 
   /**
    * Initiate Pure Counter
@@ -405,44 +426,43 @@
 
   //
 
-  var form = document.getElementById('my-form');
+  var form = document.getElementById("my-form");
 
   async function handleSubmit(event) {
     event.preventDefault();
-    var status = document.getElementById('my-form-status');
+    var status = document.getElementById("my-form-status");
     var data = new FormData(event.target);
 
-    form.querySelector('.loading').classList.add('d-block');
-    form.querySelector('.error-message').classList.remove('d-block');
-    form.querySelector('.sent-message').classList.remove('d-block');
+    form.querySelector(".loading").classList.add("d-block");
+    form.querySelector(".error-message").classList.remove("d-block");
+    form.querySelector(".sent-message").classList.remove("d-block");
 
     fetch(event.target.action, {
       method: form.method,
       body: data,
       headers: {
-        Accept: 'application/json',
+        Accept: "application/json",
       },
     })
       .then((response) => {
-        form.querySelector('.loading').classList.remove('d-block');
+        form.querySelector(".loading").classList.remove("d-block");
         if (response.ok) {
-          form.querySelector('.sent-message').classList.add('d-block');
+          form.querySelector(".sent-message").classList.add("d-block");
           form.reset();
         } else {
-          displayError(form,'Oops! There was a problem in your form');
+          displayError(form, "Oops! There was a problem in your form");
         }
-        
       })
       .catch((error) => {
         displayError(form, error);
       });
   }
 
-  form.addEventListener('submit', handleSubmit);
+  form.addEventListener("submit", handleSubmit);
 
   function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
+    thisForm.querySelector(".loading").classList.remove("d-block");
+    thisForm.querySelector(".error-message").innerHTML = error;
+    thisForm.querySelector(".error-message").classList.add("d-block");
   }
 })();
