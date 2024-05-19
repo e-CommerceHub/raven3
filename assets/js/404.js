d6743ebbcb97019696afc80bc9632158b73165f7
Patricio Monpelat <patricio.monpelat@gmail.com>
2024-05-19 19:47:22 -0300

    .

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,5 +1,10 @@
 (function () {
   "use strict";
+  var currentYear = new Date().getFullYear();
+  document.getElementById("copyright").innerHTML =
+    currentYear +
+    " © Copyright <strong><span>Agencia raven3</span></strong>. Todos los derechos reservados. Argentina, Buenos Aires.";
+
   const backButton = document.getElementById("backearth");
   backButton.addEventListener("click", () => {
     window.location.href = "https://raven3.com.ar";
