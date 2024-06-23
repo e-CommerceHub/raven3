6f67452b7bb52a850e1299bf4e4397713d26af79
Patricio Monpelat <patricio.monpelat@gmail.com>
2024-06-23 15:50:59 -0300

    .

--- assets/js/posts.js
+++ assets/js/posts.js
@@ -25,6 +25,33 @@ const posts = [
     }
   ];
 
+  const posts2 = [
+    {
+      title: "Los Secretos para una Tienda en Línea Exitosa",
+      content: "En este post, exploraremos las claves para crear una tienda en línea exitosa que atraiga a tus clientes ideales y aumente tus ventas. Si estás pensando en lanzar una tienda virtual, sigue leyendo.",
+      subtitle:"Los Secretos para una Tienda en Línea Exitosa",
+      image: "assets/img/blog/1.jpg",
+      date: "2016-03-05",
+      href:"blog-details.html"
+    },
+    {
+      title: "Qué son las palabras clave y cómo usarlas en tu tienda online",
+      content: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      subtitle:"post title 2 little longer",
+      image: "assets/img/blog/2.jpg",
+      date: "2016-03-05",
+      href:"blog-details.html"
+    },
+    {
+      title: "¿Cómo hacer publicidad en redes sociales? 10 claves para maximizar tus resultados",
+      content: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      subtitle:"post title 2 little longer",
+      image: "assets/img/blog/3.jpg",
+      date: "2016-03-05",
+      href:"blog-details.html"
+    }
+  ];
+
   const blogPostsContainer = document.getElementById("blog-posts");
   if(blogPostsContainer){
     posts.forEach((post) => {
@@ -45,6 +72,29 @@ const posts = [
       </div>
       `;
       blogPostsContainer.appendChild(postElement);
+    });
+  }
+
+  const blogPostsContainer2 = document.getElementById("blog-posts2");
+  if(blogPostsContainer2){
+    posts2.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.className = "col-lg-4 col-md-4 col-xs-12 mx-1";
+      postElement.id="PostCard";
+      postElement.innerHTML = `
+      <div class="single-blog mx-3">
+        <div class="single-blog-img p-3">
+          <a href="${post.href}"><img src="${post.image}" alt="" /></a>
+        </div>
+        <div class="blog-meta">
+          <a href="${post.href}"><span class="date-type"><i class="bi bi-calendar"></i>${post.date}</span> </a>
+        </div>
+        <div class="blog-text">
+          <h1 style="font-size: 18px;"><a href="#">${post.title}</a></h1>
+        </div>
+      </div>
+      `;
+      blogPostsContainer2.appendChild(postElement);
     });
   }
   
